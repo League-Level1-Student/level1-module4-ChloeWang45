43cae7f18176154d50d6c22d66ed4ffa8ae2036a
student <student@ip-172-31-48-170.us-west-2.compute.internal>
2021-03-31 16:56:26 -0700

    Completed SimonSays and Jeopardy.

--- src/_02_simon_says/SimonSays.java
+++ src/_02_simon_says/SimonSays.java
@@ -30,24 +30,45 @@ public class SimonSays extends KeyAdapter {
 
 	// Complete steps 1 - 7 before you test
 	// 1. Declare a JFrame variable
-
+	JFrame frame = new JFrame();
 	public void run() {
 		// 2. Add the four images that match keyboard keys like this:
-		// images.put(KeyEvent.VK_UP, "up.jpg");
-
+		images.put(KeyEvent.VK_UP, "up.jpg");
+		images.put(KeyEvent.VK_DOWN, "down.jpg");
+		images.put(KeyEvent.VK_RIGHT, "right.jpg");
+		images.put(KeyEvent.VK_LEFT, "left.jpg");
 		// 3. Use a JOptionPane to tell the user the rules: "Press the matching
 		// key when
 		// 'Simon says' otherwise press a different key"
-
+		JOptionPane.showMessageDialog(null, "Press the matching key when 'Simon says' otherwise press a different key");
 		// 4. Call the showImage method to show an image
-
+		showImage();
 	}
 
 	public void keyPressed(KeyEvent e) {
 		// 15. Make a points variable to track the score.
-
+		int points = 0;
 		// 16. If the keyCode matches the imageIndex and "Simon says"
-
+		if(e.getKeyCode() == imageIndex && simonSays == true) {
+			points = points + 1;
+			speak("Correct.");
+			tries = tries + 1;
+		}
+		else if(e.getKeyCode() != imageIndex && simonSays == true) {
+			points = points - 1;
+			speak("Simon didn't say that, so you are incorrect.");
+			tries = tries + 1;
+		}
+		else if(e.getKeyCode() != imageIndex && simonSays == false){
+			points = points + 1;
+			speak("Correct.");
+			tries = tries + 1;
+		}
+		else if(e.getKeyCode() == imageIndex && simonSays == false){
+			points = points - 1;
+			speak("Simon didn't say that, so you are incorrect.");
+			tries = tries + 1;
+		}
 		// 17. Increase the value of score
 
 		// 18. Use the speak method to tell the user they were correct
@@ -60,40 +81,51 @@ public class SimonSays extends KeyAdapter {
 		// 21. Use the speak method to tell the user they were correct
 
 		// 22. Increment tries by 1
-
+		
 		// 25. If tries is greater than 9 (or however many you want)...
-
+		if(tries >= 10) {
+			speak("Your final score is " + points);
+			System.exit(0);
+			
+		}
 		// 26. Tell the user their score
-
+		frame.dispose();
 		// 27. Exit the program
 
 		// 23. Dispose of the frame
 
 		// 24. Call the showImage method to show a new image
+		showImage();
 	}
 
 	private void showImage() {
 		// 5. Initialize your frame to a new JFrame()
-
+		frame = new JFrame();
 		// 6. Set the frame to visible
-
+		frame.setVisible(true);
 		// 7. Uncomment the following line to add a random image to your frame
-		// frame.add(getNextRandomImage());
+		frame.add(getNextRandomImage());
 
 		// 8. Set the name of your frame
-
+		frame.setName("asdf");
 		// 9. Pack the frame
-
+		frame.pack();
 		// 10. Set the defaultCloseOperation of your frame to
 		// JFrame.EXIT_ON_CLOSE
-
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// 11. Add a key listener to the frame
-
+		frame.addKeyListener(this);
 		// 12. Create a new instance of Random
-
+		Random ran = new Random();
 		// 13. Use the Random and the speak method to either say
 		// "Simon says press this key" or "Press this key"
-
+		simonSays = ran.nextBoolean();
+		if(simonSays == true) {
+			speak("Simon says press this key");
+		}
+		else {
+			speak("Press this key");
+		}
 		// 14. Above, set the value of simonSays to true/false appropriately
 
 	}
@@ -110,22 +142,22 @@ public class SimonSays extends KeyAdapter {
 	}
 
 	static void speak(String words) {
-		
-		if (System.getProperty("os.name").contains("Windows")) {
-			String cmd = "PowerShell -Command \"Add-Type -AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('"
-					+ words + "');\"";
-			try {
-				Runtime.getRuntime().exec(cmd).waitFor();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		} else {
-			try {
-				Runtime.getRuntime().exec("say " + words).waitFor();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
+		System.out.println(words);
+//		if (System.getProperty("os.name").contains("Windows")) {
+//			String cmd = "PowerShell -Command \"Add-Type -AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('"
+//					+ words + "');\"";
+//			try {
+//				Runtime.getRuntime().exec(cmd).waitFor();
+//			} catch (Exception e) {
+//				e.printStackTrace();
+//			}
+//		} else {
+//			try {
+//				Runtime.getRuntime().exec("say " + words).waitFor();
+//			} catch (Exception e) {
+//				e.printStackTrace();
+//			}
+//		}
 	}
 
 }
--- src/_03_jeopardy/Jeopardy.java
+++ src/_03_jeopardy/Jeopardy.java
@@ -35,7 +35,7 @@ import javax.swing.JPanel;
 public class Jeopardy implements ActionListener {
 	private JButton firstButton;
 	private JButton secondButton;
-	private JButton thirdButton, fourthButton;
+	private JButton thirdButton, fourthButton, fifthButton;
 	private JPanel quizPanel;
 	private int score = 0;
 	private JLabel scoreBox = new JLabel("0");
@@ -51,33 +51,42 @@ public class Jeopardy implements ActionListener {
 		frame.setLayout(new BorderLayout());
 
 		// 1. Make the frame show up
-
+		frame.setVisible(true);
 		// 2. Give your frame a title
-
+		frame.setTitle("Jeopardy");
 		// 3. Create a JPanel variable to hold the header using the createHeader method
-
+		JPanel science = createHeader("Science");
 		// 4. Add the header component to the quizPanel
-
+		quizPanel.add(science);
 		// 5. Add the quizPanel to the frame
-
+		frame.add(quizPanel);
 		// 6. Use the createButton method to set the value of firstButton
-
+		firstButton = createButton("$200");
 		// 7. Add the firstButton to the quizPanel
-
+		quizPanel.add(firstButton);
 		// 8. Write the code to complete the createButton() method below. Check that your
 		// game looks like Figure 1 in the Jeopardy Handout - http://bit.ly/1bvnvd4.
-
+		
 		// 9. Use the secondButton variable to hold a button using the createButton
 		// method
-
+		secondButton = createButton("$400");
 		// 10. Add the secondButton to the quizPanel
-
+		quizPanel.add(secondButton);
 		// 11. Add action listeners to the buttons (2 lines of code)
-
+		firstButton.addActionListener(this);
+		secondButton.addActionListener(this);
 		// 12. Write the code to complete the actionPerformed() method below
-
-		// 13. Add buttons so that you have $200, $400, $600, $800 and $1000 questions
 		
+		// 13. Add buttons so that you have $200, $400, $600, $800 and $1000 questions
+		thirdButton = createButton("$600");
+		fourthButton = createButton("$800");
+		fifthButton = createButton("$1000");
+		quizPanel.add(thirdButton);
+		quizPanel.add(fourthButton);
+		quizPanel.add(fifthButton);
+		thirdButton.addActionListener(this);
+		fourthButton.addActionListener(this);
+		fifthButton.addActionListener(this);
 		 /*
 		 * [optional] Use the showImage or playSound methods when the user answers a
 		 * question
@@ -94,14 +103,14 @@ public class Jeopardy implements ActionListener {
 	private JButton createButton(String dollarAmount) {
 		
 		// Create a new JButton
-
+		JButton button = new JButton();
 		// Set the text of the button to the dollarAmount
-
+		button.setText(dollarAmount);
 		// Increment the buttonCount (this should make the layout vertical)
-
+		buttonCount = 5;
 		// Return your new button instead of the temporary button
-
-		return new JButton("temporary button");
+		
+		return new JButton(dollarAmount);
 	}
 
 	public void actionPerformed(ActionEvent e) {
@@ -111,42 +120,61 @@ public class Jeopardy implements ActionListener {
 
 		JButton buttonPressed = (JButton) e.getSource();
 		// If the buttonPressed was the firstButton
-
+		if(buttonPressed == firstButton) {
 			// Call the askQuestion() method
- 
+			askQuestion("What is the nearest planet to the sun?", "Mercury", 200);
+			firstButton.setText(null);
 		// Complete the code in the askQuestion() method. When you play the game, the score should change.
-
+		}
 		// If the buttonPressed was the secondButton
-
+		else if(buttonPressed == secondButton) {
+			askQuestion("True or False - Lighting is hotter than the sun.", "true", 400);
+			secondButton.setText(null);
+		}
+		else if(buttonPressed == thirdButton) {
+			askQuestion("How long does a human red blood cell survive?", "120 days", 600);
+			thirdButton.setText(null);
+		}
+		else if(buttonPressed == fourthButton) {
+			askQuestion("This man is responsible for reshaping the way early man believed the solar system worked. He proposed that the Earth was not the center of the universe, and that the sun was instead at the center of our solar system. Who was he?", "Copernicus", 800);
+			fourthButton.setText(null);
+		}
+		else if(buttonPressed == fifthButton) {
+			askQuestion("What is the bone diseases that literally translates to ‘porous bones’?", "osteoporosis", 1000);
+			fifthButton.setText(null);
+		}
 			// Call the askQuestion() method with a harder question
 
 		// Clear the text on the button that was pressed (set the button text to nothing)
+		
 
 	}
 
 	private void askQuestion(String question, String correctAnswer, int prizeMoney) {
 		
 		// Use the playJeopardyTheme() method to play music while the use thinks of an answer
-		
+		playJeopardyTheme();
 		// Remove this temporary message and replace it with a pop-up that asks the user the question
-		JOptionPane.showMessageDialog(null, "this is where the question will be asked");
+		String answer = JOptionPane.showInputDialog(null, question);
 		
 		// Stop the theme music when they have entered their response. Hint: use the sound variable 
 		
 		// If the answer is correct
-
+		if(answer.equalsIgnoreCase(correctAnswer)) {
 			// Increase the score by the prizeMoney
-
+			score = score + prizeMoney;
 			// Pop up a message to tell the user they were correct
-
+			JOptionPane.showMessageDialog(null, "Correct.");
+		}
 		// Otherwise
-
+		else {
 			// Decrement the score by the prizeMoney
-
+			score = score - prizeMoney;
 			// Pop up a message to tell the user they were wrong and give them the correct answer
-
+			JOptionPane.showMessageDialog(null, "Incorrect. The correct answer is " + correctAnswer);
+		}
 		// Call the updateScore() method
-
+		updateScore();
 	}
 
 	public static synchronized void playJeopardyTheme()
