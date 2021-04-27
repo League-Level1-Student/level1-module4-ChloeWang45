ae5fefcf9849db912264b55745cd12763a4957ec
student <student@ip-172-31-56-4.us-west-2.compute.internal>
2021-04-26 18:24:24 -0700

    Completed WhackAMole.

--- src/_11_whack_a_mole/WhackAMole.java
+++ src/_11_whack_a_mole/WhackAMole.java
@@ -1,68 +1,82 @@
 package _11_whack_a_mole;
 
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Date;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class WhackAMole implements ActionListener {
 
 	JPanel panel = new JPanel();
 	JFrame frame = new JFrame();
-	JButton a = new JButton();
-	JButton b = new JButton();
-	JButton c = new JButton();
-	JButton d = new JButton();
-	JButton e = new JButton();
-	JButton f = new JButton();
-	JButton g = new JButton();
-	JButton h = new JButton();
-	JButton i = new JButton();
-	JButton j = new JButton();
-	JButton k = new JButton();
-	JButton l = new JButton();
+	JButton mole = new JButton();
+	Random rand = new Random();
+	GridLayout layout = new GridLayout(8, 3);
+	Date date = new Date();
+	int missed = 0;
+	int whacked = 0;
 	
 	public WhackAMole() {
 		
-		a.addActionListener(this);
-		b.addActionListener(this);
-		c.addActionListener(this);
-		d.addActionListener(this);
-		e.addActionListener(this);
-		f.addActionListener(this);
-		g.addActionListener(this);
-		h.addActionListener(this);
-		i.addActionListener(this);
-		j.addActionListener(this);
-		k.addActionListener(this);
-		l.addActionListener(this);
-		panel.add(a);
-		panel.add(b);
-		panel.add(c);
-		panel.add(d);
-		panel.add(e);
-		panel.add(f);
-		panel.add(g);
-		panel.add(h);
-		panel.add(i);
-		panel.add(j);
-		panel.add(k);
-		panel.add(l);
-		frame.add(panel);
-		frame.setSize(500, 500);
-		frame.setVisible(true);
+		drawButtons();
 		
 	}
 	
-	void drawButtons(int mole) {
+	void drawButtons() {
+		frame = new JFrame();
+		panel = new JPanel();
+		panel.setLayout(layout);
+		int moleNum = rand.nextInt(24);
+			for(int i = 0; i < 24; i++) {
+				JButton button = new JButton();
+				button.addActionListener(this);
+				panel.add(button);
+				if(moleNum == i) {
+					mole = button;
+					button.setText("Mole");
+				}
+			}		
+			frame.add(panel);
+			frame.setSize(300, 300);
+			frame.setVisible(true);
+			
+		}
 		
-	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
+		JButton buttonPressed = (JButton) arg0.getSource();
+		if(mole == buttonPressed) {
+			whacked++;
+		}
+		else {
+			System.out.println("YOU MISSED!!");
+			missed++;
+		}
+		frame.dispose();
 		
+		if(missed == 5 || missed + whacked == 10) {
+			if(missed == 5) {
+				JOptionPane.showMessageDialog(null, "YOU LOST!!");
+			}
+			endGame(date, whacked);
+		}
+		else {
+			drawButtons();
+		}
 	}
+	
+	 private void endGame(Date timeAtStart, int molesWhacked) { 
+		    Date timeAtEnd = new Date();
+		    JOptionPane.showMessageDialog(null, "Your whack rate is "
+		            + ((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000.00 / molesWhacked)
+		                  + " moles per second.");
+		}
 }
